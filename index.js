4b33ddde918786e1eb9dbbeffc2c5178fbbfca81
Brian Bartholomew <me@bcbrian.com>
2019-09-25 21:09:28 -0500

    got the light to turn on!

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-function damo(anything) {
-  alert(anything);
+function damoLightPull() {
+  const lightPullEl = document.getElementById("light-pull-string");
+  const lightEl = document.getElementById("light");
+
+  lightPullEl.addEventListener("click", function() {
+    lightEl.classList.toggle("on");
+  });
 }
+
+damoLightPull();
